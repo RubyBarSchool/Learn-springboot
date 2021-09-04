676bb5a95d6811c1ef1f3ef2c18d1dd201678931
tranvutruong <truongtv1399it@gmail.com>
2021-09-04 07:48:29 +0700

    update demo function

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,76 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+		List<Person> personList = List.of(
+				new Person("truong",Gender.MALE),
+				new Person("ngoc",Gender.MALE),
+				new Person("A",Gender.FEMALE),
+				new Person("b",Gender.FEMALE),
+				new Person("c",Gender.MALE),
+				new Person("d",Gender.FEMALE),
+				new Person("e",Gender.MALE)
+		);
+		System.out.println("Imperative Approach");
+		// Imperative Approach
+		List<Person> personListFemale = new ArrayList<>();
+		for(Person person : personList){
+			if(person.gender.equals(Gender.FEMALE)){
+				personListFemale.add(person);
+			}
+		}
+
+		for(Person person : personListFemale){
+			System.out.println(person.toString());
+		}
+
+		System.out.println("Declarative Approach");
+		Predicate<Person> personPredicate = person -> (person.gender.equals(Gender.FEMALE));
+		//Declarative Approach
+		personList.stream()
+				.filter(person -> (person.gender.equals(Gender.FEMALE)))
+				.collect(Collectors.toList())
+				.forEach(System.out::println);
+
+		List<Person> personListFemalex = personList.stream()
+				.filter(personPredicate)
+				.collect(Collectors.toList());
+
+		//Function
+		System.out.println("Declarative Approach");
+		Function<Integer,Integer> add = integer -> ++integer ;
+		System.out.println(add.apply(1));
+	}
+	static  class Person{
+		private String name;
+		private Gender gender;
+
+		public Person() {
+		}
+
+		public Person(String name, Gender gender) {
+			this.name = name;
+			this.gender = gender;
+		}
+
+		@Override
+		public String toString() {
+			return "Person{" +
+					"name='" + name + '\'' +
+					", gender=" + gender +
+					'}';
+		}
+	}
+	enum Gender{
+		MALE,FEMALE
 	}
 }
